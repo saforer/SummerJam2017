6e4b824a8635f93994ae50c9951a02839d8ea5c3
saforer <saforer@gmail.com>
2017-07-08 11:19:49 -0400

    fuck I think I actually need to enter the summer jam now I have something.

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,13 +4,97 @@ using UnityEngine;
 
 public class Player : MonoBehaviour {
 
+    public float speed;
+    public float jumpStrength;
+
+    bool grounded = false;
+
+    bool jumped = false;
+    bool facingRight = true;
+
+    public Rigidbody2D rb;
+    public Animator anim;
+    public BoxCollider2D jumpBox;
+    public GameObject fireballPosition;
+
+    private PlayerWeaponStates currentMehrio = PlayerWeaponStates.big;
+    
+
 	// Use this for initialization
 	void Start () {
-		
+        
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+
+        HitboxUpdate();
+
+        ControlUpdate();
+        
+        AnimationUpdate();
 	}
+
+    void HitboxUpdate()
+    {
+        bool temp = false;
+        if (!grounded) temp = false;
+        grounded = jumpBox.IsTouchingLayers(LayerMask.GetMask("Ground"));
+        if (!temp && grounded)
+        {
+            jumped = false;
+        }        
+    }
+
+    void ControlUpdate()
+    {
+        rb.velocity = new Vector2(Input.GetAxis("Horizontal") * speed, rb.velocity.y);
+
+        if (Input.GetAxis ("Vertical") > 0.3f)
+        {
+            JumpMehrio();
+        }
+    }
+
+    void AnimationUpdate()
+    {
+        anim.SetFloat("speed", Mathf.Abs(Input.GetAxis("Horizontal")));
+        
+        //Facing direction
+        if (facingRight && (Input.GetAxis("Horizontal") < 0))
+        {
+            facingRight = false;
+            FlipSprite();
+        } else if (!facingRight && (Input.GetAxis("Horizontal") > 0))
+        {
+            facingRight = true;
+            FlipSprite();
+        }
+
+        anim.SetBool("grounded", grounded);
+    }
+
+    void FlipSprite()
+    {
+        SpriteRenderer sr = GetComponent<SpriteRenderer>();
+        bool temp = sr.flipX;
+        sr.flipX = !temp;
+    }
+
+    void JumpMehrio()
+    {
+        if (grounded && !jumped)
+        {
+            jumped = true;
+            Vector2 jVector = new Vector2(0, jumpStrength);
+            rb.AddForce(jVector);
+        }
+    }
+}
+
+public enum PlayerWeaponStates
+{
+    fireball,
+    big,
+    small
 }
---
+++ Assets/Scripts/PlayerTracker.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerTracker : MonoBehaviour {
+    Camera cam;
+    public Transform player;
+    public float speed;
+    public float edge;
+
+	// Use this for initialization
+	void Start () {
+        cam = GetComponent<Camera>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Vector3 screenPos = cam.WorldToScreenPoint(player.position);
+        float percentOnScreen = screenPos.x / cam.pixelWidth;
+        percentOnScreen -= .50f;
+        percentOnScreen *= 2f;
+        Debug.Log("target is " + percentOnScreen + "%");
+
+
+
+        if (percentOnScreen >= edge)
+        {
+            transform.position += new Vector3(speed * percentOnScreen, 0, 0);
+        } else if (percentOnScreen <= -edge)
+        {
+            if (transform.position.x > 1.18f) transform.position += new Vector3(speed * percentOnScreen * percentOnScreen * percentOnScreen, 0, 0);
+        }
+        
+	}
+}
